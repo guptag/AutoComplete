b508a77a2499db024791a987783f5f944c7499b2
Gupta Garuda <guptagaruda@gmail.com>
2013-07-09 16:01:25 -0400

    AutoComplete demo page

---
+++ autocomplete.js
@@ -0,0 +1,231 @@
+(function () {
+    var w = window;
+    w.controls = w.controls || {};
+    w.controls.$AutoComplete = function () {
+        // private variables
+        var _$input, _dataSrc;
+        var _this = this;
+
+        constr(arguments[0]);
+
+        _this.init = function () {
+            // initialize the data source
+            // this will fireup a worker thread and builds Trie
+            _dataSrc.init();
+            attachEvents();
+        }
+
+        function constr(p_args) {
+            _$input = $(p_args.id);
+            _dataSrc = p_args.dataSrc;
+            _$input.wrap('<div class="autocomplete"></div>');
+        }
+
+        function attachEvents() {
+            // bind all the needed events
+            _$input.bind("focus.autocomplete blur.autocomplete", toggleFocus);
+            _$input.bind("keyup.autocomplete", handleKeyup);
+            _$input.bind("keydown.autocomplete", handleKeydown);
+            $(window).bind("unload beforeunload", function () {
+                _$input.unbind(".autocomplete");
+            });
+        }
+
+        function toggleFocus(p_ev) {
+            if (p_ev.type == "focus") {
+                _$input.addClass("focus");
+            }
+            else {
+                _$input.removeClass("focus");
+                toggleSuggestions();
+            }
+        }
+
+        function handleKeyup(p_ev) {
+            var _input = _$input.val();
+
+            // if text is cleared out - hide the suggestions
+            if (_input == "") {
+                _dataSrc.clearPendingQueries();
+                toggleSuggestions();
+            }
+            else
+                if (p_ev.keyCode == 13) //enter key
+                {
+                    _dataSrc.clearPendingQueries();
+                    var _$activeLi = $("#" + _$input.attr("id") + "_ul li.active");
+                    if (_$activeLi.length == 1) {
+                        // if an active list element exists, enter it's value in the textbox
+                        _$input.val(_$activeLi.text());
+                    }
+                    toggleSuggestions();
+                }
+                else if (p_ev.keyCode == 8 || (p_ev.keyCode >= 65 && p_ev.keyCode <= 90)) //back arrow or alphabets
+                {
+                    // findMatches is an async api which returns promise object (instead of the final result)
+                    // attach the callback to the promise object using .then
+                    _dataSrc.findMatches(_input)
+                        .then(onResultSet);
+                }
+        }
+
+
+        function handleKeydown(p_ev) {
+            var _input = _$input.val();
+
+            if (p_ev.keyCode == 40) //down key
+            {
+                // scroll down the list of suggestions
+                // start with the first element if the selection reaches to the end
+                var _$activeLi = $("#" + _$input.attr("id") + "_ul li.active");
+                var _$tobeActive = $("#" + _$input.attr("id") + "_ul li").first();
+                if (_$activeLi.length == 1) {
+                    _$activeLi.removeClass("active");
+
+                    var _$next = _$activeLi.next();
+                    if (_$next.length == 1) {
+                        _$tobeActive = _$next;
+                    }
+                }
+                _$tobeActive.addClass("active").focus();
+            }
+            else
+                if (p_ev.keyCode == 38) //up key
+                {
+                    // scroll up the list of suggestions
+                    // start with the last element if the selection reaches to the top
+                    var _$activeLi = $("#" + _$input.attr("id") + "_ul li.active");
+                    var _$tobeActive = $("#" + _$input.attr("id") + "_ul li").last();
+                    if (_$activeLi.length == 1) {
+                        _$activeLi.removeClass("active");
+
+                        var _$prev = _$activeLi.prev();
+                        if (_$prev.length == 1) {
+                            _$tobeActive = _$prev;
+                        }
+                    }
+                    _$tobeActive.addClass("active").focus();
+                }
+        }
+
+        function onResultSet(p_data) {
+            // build the suggestion for the result
+            var _result = p_data.status === "success" ? p_data.result : [];
+            toggleSuggestions(p_data.result);
+        }
+
+        function toggleSuggestions(p_suggestions) {
+            // Clean up the dom			
+            $("#" + _$input.attr("id") + "_ul").unbind(".menuevents").remove();
+
+            // Append the dom with new suggestions
+            if (p_suggestions && p_suggestions.length > 0) {
+                var _sugDom = $('<ul id=' + _$input.attr("id") + '_ul></ul>')
+                                    .css({ "min-width": _$input.outerWidth() - 2 })
+                                    .bind("mouseover.menuevents mouseout.menuevents", toggleMenuItemFocus);
+
+                $.each(p_suggestions, function () {
+                    _sugDom.append($(["<li>", this, "</li>"].join("")));
+                });
+                _$input.parent().append(_sugDom);
+            }
+        }
+
+        function toggleMenuItemFocus(p_ev) {
+            // mouseover, mouseout events on the suggestion list - change the highlight
+            $("#" + _$input.attr("id") + "_ul li.active").removeClass("active");
+            $(p_ev.srcElement).addClass("active");
+        }
+    };
+})();
+
+(function () {
+    var w = window;
+    w.data = w.data || {};
+    w.data.$DataHandler = function (p_srcUrl) {
+        var _this = this;
+        var c_MaxResults = 25;
+        var _srcUrl = p_srcUrl;
+        var _initCb, _webWorker, _isTrieReady, _isInitialized;
+        var _deferred;
+        var _queuedQueries = [];
+
+        _this.init = function () {
+            if (!_isInitialized) {
+                _isInitialized = true;
+
+                // Make an ajax call and send the data to the webworker for trie generation
+                $.ajax("scrabble.txt")
+                 .done(function (p_data) {
+                     // initialize webworker
+                     if (!_webWorker) {
+                         _webWorker = new Worker("worker.js");
+
+                         _webWorker.addEventListener("message", onMessageFromWorker, false);
+
+                         //Send the words to the webworker (remove the first one which is a comment)
+                         _webWorker.postMessage({ "cmd": "inittrie", "pckg": { "data": p_data.split('\n').slice(1)} });
+                     }
+                 });
+            }
+        }
+
+        _this.findMatches = function (p_query) {
+            if (_isTrieReady && !_deferred) {
+                // send a message to worker thread to get the matches from the query (for the text entered so far)
+                _webWorker.postMessage({ "cmd": "findmatches", "pckg": { "query": p_query, "max": c_MaxResults} });
+                _deferred = $.Deferred();
+            }
+            else {
+                // queue the query requets (worer thread is churning the previous query)
+                _queuedQueries.push(p_query);
+            }
+
+            // return the promise object 
+            // all the callbacks associated with this promise will be executed when the deferred is resolved			   
+            return _deferred.promise();
+        }
+
+        _this.dispose = function () {
+            // dispose the worker
+            _worker.terminate();
+        }
+
+        _this.clearPendingQueries = function () {
+            _queuedQueries.length = 0;
+            if (_deferred) {
+                _deferred = null;
+            }
+        }
+
+        // handle the message from web worker
+        function onMessageFromWorker(p_ev) {
+            var _data = p_ev.data;
+            var _successStr = "success";
+
+            if (!(_data && _data.cmd)) return;
+
+            if (_data.cmd === "inittrie" && _data.status == _successStr) {
+                //mark the state
+                _isTrieReady = true;
+            }
+            else
+                if (_data.cmd === "findmatches") {
+                    if (_deferred && _data.status == _successStr) {
+                        // handle the client callbacks
+                        _deferred.resolve({ "status": _successStr, "result": _data.result });
+                        _deferred = null;
+                    }
+
+                    // handle the pending queries
+                    if (_queuedQueries.length > 0) {
+                        var _recentQuery = _queuedQueries.pop();
+                        _queuedQueries.length = 0;
+                        _this.findMatches(_recentQuery);
+                    }
+
+                }
+        }
+    }
+})();
+
---
+++ worker.js
@@ -0,0 +1,113 @@
+self.addEventListener("message", function (p_ev) {
+    var _data = p_ev.data;
+    if (!(_data && _data.cmd && _data.pckg)) return;
+
+    // helper method to post the message to UI thread
+    var _postMessage = function (p_cmd, p_status, p_result) {
+        self.postMessage({ "cmd": p_cmd, "status": p_status, "result": p_result });
+    }
+
+    if (_data.cmd == "inittrie") {
+        if (!self._trie) {
+            try {
+                self._trie = new self.data.$Trie(_data.pckg.data);
+                _postMessage(_data.cmd, "success");
+            }
+            catch (e) {
+                _postMessage(_data.cmd, "error", e.message);
+            }
+        }
+
+    }
+    else
+        if (_data.cmd == "findmatches") {
+            if (self._trie) {
+                if (_data.pckg.query && _data.pckg.max) {
+                    var _results = self._trie.findMatches(_data.pckg.query, _data.pckg.max);
+                    _postMessage(_data.cmd, "success", _results);
+                }
+                else {
+                    _postMessage(_data.cmd, "error", "invalid input");
+                }
+            }
+            else {
+                _postMessage(_data.cmd, "error", "trie not ready yet");
+            }
+        }
+}, false);
+
+
+
+(function () {
+    var w = self;
+    w.data = w.data || {};
+    w.data.$Trie = function (p_data) {
+        var _this = this;
+
+        // capture the argument and build the Trie
+        constr((arguments[0] && arguments[0].constructor === Array) ? arguments[0] : []);
+
+        function constr(p_data) {
+            _this.data = p_data;
+            _this.root = {};
+            buildTrie();
+        }
+
+        function buildTrie() {
+            // build the trie structure for the input array
+            var _index, _count = _this.data.length;
+            for (_index = 0; _index < _count; ++_index) {
+                var _word = _this.data[_index], _charCount = _word.length;
+                var _charIndex, _root = _this.root;
+
+                // for each character in the word, create a chain of nodes from the root
+                for (_charIndex = 0; _charIndex < _charCount; ++_charIndex) {
+                    var _char = _word.charAt(_charIndex);
+                    if (!_root[_char]) {
+                        _root[_char] = {};
+                    }
+                    _root = _root[_char];
+                }
+            }
+        }
+
+        _this.findMatches = function (p_query, p_maxCount) {
+            //find all the words in the Trie that starts with p_query ("ba" -> "bat", "balm", "bad", "badge"....)
+            var _results = [], _resultStr = "";
+            var _root = this.root, _rootChar;
+
+            // go down the tree to find a root node which matches the p_query
+            for (var _index = 0; _index < p_query.length; ++_index) {
+                _rootChar = p_query.charAt(_index);
+                _root = _root[_rootChar];
+                if (!_root) {
+                    return [];
+                }
+            }
+
+            // if such root exists - find all the words from this root (traverse the sub-tree using DFS)
+            if (_root) {
+                var _subResults = getResultsForSubtree(_rootChar, _root);
+                for (var i = 0; i < _subResults.length; ++i) {
+                    _results.push(p_query.slice(0, p_query.length - 1) + _subResults[i]);
+                }
+            }
+
+            return typeof (p_maxCount != undefined && p_maxCount < _results.length) ? _results.slice(0, p_maxCount) : _results;
+        }
+
+        function getResultsForSubtree(p_rootText, p_root) {
+            // find all the words from p_rootText (traverse all the nodes from this root using DFS)
+            var _results = [];
+            for (var _p in p_root) {
+                if (p_root.hasOwnProperty(_p)) {
+                    var _tempResults = getResultsForSubtree(_p, p_root[_p]);
+                    for (var i = 0; i < _tempResults.length; ++i) {
+                        _results.push(p_rootText + _tempResults[i]);
+                    }
+                }
+            }
+            return _results.length > 0 ? _results : [p_rootText];
+        }
+    };
+})();
